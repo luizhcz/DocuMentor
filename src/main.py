2ed132948498e5c76f3080cb2656249ea7a48b0f
Luiz H <luizcerqueira1512@gmail.com>
2024-11-09 17:51:18 -0300

    novo processo

--- src/main.py
+++ src/main.py
@@ -1,7 +1,10 @@
-from fastapi import FastAPI, UploadFile, File, Form
+import os
+from fastapi import FastAPI, UploadFile, File, Form, HTTPException
 from services.pdf_service import PDFService
 from services.image_service import ImageService
 from services.text_service import TextService
+from services.crew_service import CrewService
+from services.process_service import ProcessService
 
 app = FastAPI()
 
@@ -9,11 +12,33 @@ app = FastAPI()
 pdf_service = PDFService()
 image_service = ImageService()
 text_service = TextService()
+crew_service = CrewService()
+process_service = ProcessService(crew_service=crew_service)
 
 @app.post("/upload/pdf")
 async def upload_pdf(file: UploadFile = File(...)):
-    result = await pdf_service.process(file)
-    return {"message": "PDF processed", "details": result}
+
+    try:
+        result = await pdf_service.process(file)
+
+        #Chamar CrewAI
+        agent_path = os.path.join("jsons", "agent", "get_info_prospect.json")
+        example_path = os.path.join("jsons", "example", "get_info_prospect.json")
+
+        return result 
+
+        # Process the text with CrewAI
+        processed_data = process_service.process_principal_text(
+            content=data.text,
+            agent_path=agent_path,
+            example_path=example_path
+        )
+
+        return {"message": "PDF processed", "details": result }
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
+    #Extrair pdf
+    
 
 @app.post("/upload/image")
 async def upload_image(file: UploadFile = File(...)):
---
+++ src/workers/crew_setup.py
@@ -0,0 +1,72 @@
+from crewai import Agent, Task, Crew
+from langchain_openai import ChatOpenAI
+
+class CrewSetup:
+    def __init__(self, model_name: str = "gpt-4o-mini", temperature: float = 0.1, max_execution_time: int = 999999):
+        """
+        Initializes the CrewSetup with default configurations for creating Agent, Task, and Crew.
+        
+        :param model_name: Name of the LLM model to be used in the Agent.
+        :param temperature: Temperature setting for the LLM model.
+        :param max_execution_time: Maximum execution time for the Agent, Task, and Crew.
+        """
+        self.model_name = model_name
+        self.temperature = temperature
+        self.max_execution_time = max_execution_time
+
+    def create_agent(self, role: str, goal: str, backstory: str, verbose: bool = True, memory: bool = True, max_iter: int = 20) -> Agent:
+        """
+        Creates and returns an instance of Agent.
+
+        :param role: Role of the agent.
+        :param goal: Main goal of the agent.
+        :param backstory: Agent's backstory for context.
+        :param verbose: Determines if the agent should display additional information during processing.
+        :param memory: Determines if the agent should maintain memory.
+        :param max_iter: Maximum number of iterations the agent can perform.
+        :return: Configured instance of Agent.
+        """
+        llm = ChatOpenAI(model_name=self.model_name, temperature=self.temperature)
+        
+        return Agent(
+            role=role,
+            goal=goal,
+            verbose=verbose,
+            memory=memory,
+            max_iter=max_iter,
+            backstory=backstory,
+            llm=llm,
+            max_execution_time=self.max_execution_time
+        )
+
+    def create_task(self, description: str, expected_output: str, agent: Agent) -> Task:
+        """
+        Creates and returns an instance of Task associated with the Agent.
+
+        :param description: Description of the task.
+        :param expected_output: Example of the expected output for the task.
+        :param agent: Agent responsible for performing the task.
+        :return: Configured instance of Task.
+        """
+        return Task(
+            description=description,
+            expected_output=expected_output,
+            agent=agent,
+            max_execution_time=self.max_execution_time
+        )
+
+    def create_crew(self, agents: list, tasks: list, verbose: int = 2) -> Crew:
+        """
+        Creates and returns an instance of Crew with configured agents and tasks.
+
+        :param agents: List of agents that will be part of the Crew.
+        :param tasks: List of tasks that the Crew will execute.
+        :param verbose: Verbosity level during execution.
+        :return: Configured instance of Crew.
+        """
+        return Crew(
+            agents=agents,
+            tasks=tasks,
+            verbose=verbose,
+            max_execution_time=self.max_execution_time
+        )
